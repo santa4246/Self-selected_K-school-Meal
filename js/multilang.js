2c377e0ba06f9c016e8d2d3d954dc60c82ad7587
91sheep <endakf55@gmail.com>
2024-11-22 15:22:05 +0900

    자율선택급식 영어 수정

--- js/multilang.js
+++ js/multilang.js
@@ -1,15 +1,15 @@
 const translations = {
 	en: {
 		index_title1: "Self-"
-        , index_title2: "selection<br>"
-        , index_title3: " K-School Meal programs"
+        , index_title2: "selected<br>"
+        , index_title3: " K-school Meal Programs"
         , index_title4: " Experience"
 		, index_title_desc1: "The students who are the main agents<br>of Korean school meals (autonomously)"
 		, index_title_desc2: "In a nutritional balance"
 		, index_title_desc3: "Choose Healthy Food"
 		, index_content_desc1: "학생의 자율권과 선택권을 확대하여 개개인의 필요한 영양량에 맞는 다양한 식단을 제공하여 <br>스스로 선택하게 함으로써 주도적으로 건강한 식습관을 실천할 수 있는 경기도교육청 정책"
 		, index_content_desc2: "미래학교급식으로의 발전방향이며, 학생들이 개인의 식사관리 역량을 넘어 <br>공동체성 기반 지속가능한 실천적 식생활관리역량을 함양할 수 있는 경기도교육청 정책"
-		, index_produce: "Produced by nutrition teachers Seo A-ram, Cho Hyo-hyun, and Cha Seul-gi<br>/ Development advisor Kang Minhyeok"
+		, index_produce: "Produced by nutrition teachers Seo Ah-ram, Cho Hyo-hyun, and Cha Seul-gi<br>/ Development advisor Kang Min-hyeok"
 	},
 	ko: {
 		index_title1: "자율"
